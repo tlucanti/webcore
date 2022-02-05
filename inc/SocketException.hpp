965e61f0a4b2392d93c8cecf9e2683e4888fa5f0
tlucanti <prototype3628800@yandex.ru>
2022-02-06 00:20:50 +0300

    parser started

---
+++ inc/SocketException.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   SocketException.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlucanti <tlucanti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/03 15:43:54 by tlucanti          #+#    #+#             */
+/*   Updated: 2022/02/04 14:11:04 by tlucanti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOCKET_EXCEPTION_HPP
+# define SOCKET_EXCEPTION_HPP
+
+#include "IRCException.hpp"
+
+namespace tlucanti
+{
+	class SocketException : public IRCException
+	{
+	public:
+		SocketException(const std::string &message, int error)
+				: IRCException("socket", message, error) {}
+	};
+}
+
+#endif	// SOCKET_EXCEPTION_HPP
