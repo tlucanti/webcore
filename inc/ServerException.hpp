2d03d78e30faed17f0881bc43947850a14efc288
tlucanti <prototype3628800@yandex.ru>
2022-02-06 00:20:50 +0300

    parser started

---
+++ inc/ServerException.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerException.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlucanti <tlucanti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/04 14:00:01 by tlucanti          #+#    #+#             */
+/*   Updated: 2022/02/04 14:18:03 by tlucanti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_EXCEPTION_HPP
+# define SERVER_EXCEPTION_HPP
+
+#include "IRCException.hpp"
+
+namespace tlucanti
+{
+	class ServerException : public IRCException
+	{
+	public:
+		ServerException(const std::string &message, int error)
+				: IRCException("server", message, error)  {}
+	};
+}
+
+#endif	// SERVER_EXCEPTION_HPP
