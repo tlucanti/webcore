7663377e0700c31b2f8db0395307d4500ad2625e
tlucanti <prototype3628800@yandex.ru>
2022-02-06 23:50:58 +0300

    parser done

--- inc/defs.h
+++ inc/defs.h
@@ -6,7 +6,7 @@
 /*   By: tlucanti <tlucanti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 12:09:17 by kostya            #+#    #+#             */
-/*   Updated: 2022/02/03 16:02:05 by tlucanti         ###   ########.fr       */
+/*   Updated: 2022/02/06 17:43:15 by tlucanti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,16 @@
 #  define __DEFAULT {}
 #  define __UNUSED __attribute__((unused))
 #  define __NORET __attribute__((noreturn))
+#  define __DELETE ;
+#  define __DELETED_MEMBERS private
 
 # else
 #  define __WUR [[nodiscard]]
 #  define __DEFAULT =default;
 #  define __UNUSED [[maybe_unused]]
 #  define __NORET [[noreturn]]
+#  define __DELETE =delete;
+#  define __DELETED_MEMBERS public
 
 # endif /* 199711L */
 #endif /* DEFS_H */
